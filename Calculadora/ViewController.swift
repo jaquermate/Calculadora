0a0b3921342dbd0d8053130d741e8b1d8c12aa80
Jesus Martínez de Juan <jmartinezdejuan@macbook-pro-de-jesus.local>
2017-09-26 09:14:35 +0200

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Jesus Martínez de Juan on 26/9/17.
+//  Copyright © 2017 Jesus Martínez de Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
