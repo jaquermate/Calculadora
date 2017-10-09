1bf3ac2807b0f5ccf15d6c70edbd4f6ed063cb8d
SiniorYiesus Martínez de Juan <jmartinezdejuan@MacBook-Pro-de-Jesus.local>
2017-10-09 22:34:21 +0200

    Primer commit  de la calculadora. Funcionan las operaciones básicas. Estoy peleándome con numero e y pi.

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -7,9 +7,98 @@
 //
 
 import UIKit
-
+import Darwin
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pantalla: UILabel!
+    @IBOutlet var miniPantalla: UILabel!
+    var ans : Double = 0;
+    var elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero : Bool = false
+    var op1: Double = 0
+    var op2: Double = 0
+    var operacion: String = ""
+    
+    
+   
+    func realizaOperacion()-> Double{
+        var resultado: Double = 0
+        switch operacion {
+        case "+":
+            resultado=op1+op2;
+        case "-":
+            resultado=op1-op2;
+        case "*":
+            resultado=op1*op2;
+        case "/":
+            resultado=op1/op2;
+        case "^":
+            resultado=pow(op1,op2)
+        case "√":
+            resultado = pow(op1,(1/op2))
+        default:
+            resultado=999
+        }
+        return resultado
+    }
+    
+    
+    @IBAction func numeroPulsado(_ sender: UIButton) {
+        let digito : String! = sender.currentTitle
+        
+        if elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero{
+            switch digito {
+            case "e":
+                pantalla.text = "e"
+            case "π":
+                pantalla.text = "π"
+            default:
+                pantalla.text = pantalla.text! + digito
+            }
+        }else{
+            pantalla.text = digito
+            elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero = true
+        }
+        
+        print(digito)
+        
+    }
+   
+    @IBAction func operacionPulsada(_ sender: UIButton) {
+        var numero: NSString = pantalla.text! as NSString
+        if numero == "e"{
+            op1=M_E
+        }else{if numero == "π"{
+            op1=Double.pi
+        }else {op1 = numero.doubleValue}
+                }
+        operacion = sender.currentTitle!
+        miniPantalla.text = pantalla.text
+        pantalla.text = "0"
+        elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero = false
+    }
+    @IBAction func enterPulsado() {
+        var numero: NSString = pantalla.text! as NSString
+        if numero == "e"{
+            op2=M_E
+        }else{if numero == "π"{
+            op2=Double.pi
+        }else {op2 = numero.doubleValue}
+        }
+        var resultado: Double = realizaOperacion()
+        pantalla.text = String (format: "%g", resultado)
+        ans = resultado
+        op1=0
+        elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero=false
+    }
+    @IBAction func cPulsado(_ sender: UIButton) {
+        op1 = 0;
+        op2 = 0;
+        miniPantalla.text="0"
+        pantalla.text="0"
+    }
+   
+    @IBAction func ansPulsado(_ sender: UIButton) {
+        pantalla.text = String (format : "%g", ans)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
